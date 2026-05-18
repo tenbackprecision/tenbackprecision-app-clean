206fff38f04f403321b8280e135039793f7c22cb
Cheneil Lopez <tenbackprecision@gmail.com>
2026-05-17 23:21:45 -0700

    Update v1113 and performance improvements

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import {
 import { auth, db } from "./firebase";
 import heic2any from "heic2any";
 
-const APP_VERSION = "v1112";
+const APP_VERSION = "v1113";
 const MAX_RECEIPT_SIZE_MB = 8;
 
 const expenseCategories = [
